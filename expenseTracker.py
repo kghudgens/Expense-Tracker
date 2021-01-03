abf904664647922ef75ff5c2839bfda5ae658f9d
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-03 20:54:17 +0900

    started inform user function

--- expenseTracker.py
+++ expenseTracker.py
@@ -1,17 +1,24 @@
 items = []
 cost_of_items = []
 
+
+def inform_user():
+    """Let user know what they are spending their money on"""
+    pass
+
+
 def get_total_expenses(dictionary):
     """Get the price of all expenses """
-    
+
     total = 0
 
     cost_list = list(dictionary.values())
 
     for x in cost_list:
         total = int(x) + total
-    
+
     return total
+
 
 def add_to_list(item, cost):
     """Adds user input to list"""
@@ -20,7 +27,7 @@ def add_to_list(item, cost):
     cost_of_items.append(cost)
 
     make_dictionary()
-    
+
 
 def make_dictionary():
     """
@@ -30,22 +37,22 @@ def make_dictionary():
     item_cost = dict(zip(items, cost_of_items))
     return item_cost
 
+
 def get_user_input():
     """Get users input their expenses"""
 
     while True:
         user_item = input("What did you buy? ")
-        if user_item == 'q':
+        if user_item == "q":
             break
 
         user_item_cost = input("How much did it cost you? ")
-        if user_item_cost =='q':
+        if user_item_cost == "q":
             break
-        
+
         add_to_list(user_item, user_item_cost)
         continue
-        
-    
+
 
 get_user_input()
 all_expenses = make_dictionary()
